d716071b5388b44c0debdc6a9c977b5a8e358687
songjoonhyuk <songjoonhyuk@gmail.com>
2016-05-25 23:10:01 +0900

    add blog app

--- messages/urls.py
+++ messages/urls.py
@@ -19,13 +19,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import HttpResponse
 
-def hello(request):
-    return HttpResponse('''
-        <h1>Hello, <a href="http://facebook.com/askdjango/" target="_blank">AskDjango</a></h1>
-    ''')
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', 'blog.views.index'),
     url(r'^$' , hello),
 ]
 
