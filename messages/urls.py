293870f4e5d09ab839a53d74d3cc69d890eae56a
songjoonhyuk <songjoonhyuk@gmail.com>
2016-05-25 23:11:39 +0900

    index url

--- messages/urls.py
+++ messages/urls.py
@@ -19,11 +19,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import HttpResponse
 
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', 'blog.views.index'),
-    url(r'^$' , hello),
+    url(r'^$' , 'blog.views.index'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
