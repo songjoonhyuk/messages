9f51eb413a1aa17d44f27b51131147313f65a953
songjoonhyuk <songjoonhyuk@gmail.com>
2016-06-07 07:31:05 +0900

    index에 포스트 리스트 추가

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 def index(request):
-	return render(request, 'blog/index.html')
+	post_list = Post.objects.all()
+	return render(request, 'blog/index.html', {'post_list':post_list})
