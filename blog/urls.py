2ce56c6cff3ea48351d489b1d99398780d20d525
songjoonhyuk <songjoonhyuk@gmail.com>
2016-06-08 04:19:36 +0900

    blog 앱이 urls.py 연결

---
+++ blog/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import url
+
+urlpatterns = [
+	url(r'^(?P<pk>\d+)$', 'blog.views.post_detail', name='post_detail'),
+]
--- messages/urls.py
+++ messages/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import HttpResponse
@@ -22,6 +22,7 @@ from django.http import HttpResponse
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$' , 'blog.views.index'),
+    url(r'^blog/', include('blog.urls', namespace='blog')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
