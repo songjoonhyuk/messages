f4903a4c71ae0c18b6b37f4cd63a2a9da47cb516
songjoonhyuk <songjoonhyuk@gmail.com>
2016-06-08 04:25:24 +0900

    post detail 페이지 완성

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,8 @@ from .models import Post
 
 def index(request):
 	post_list = Post.objects.all()
-	return render(request, 'blog/index.html', {'post_list':post_list})
+	return render(request, 'blog/index.html', {'post_list':post_list, })
+
+def post_detail(request, pk):
+	post = Post.objects.get(pk=pk)
+	return render(request, 'blog/post_detail.html', {'post':post, })
