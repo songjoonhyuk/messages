f478838110eabf1c44970450e671dcc20877e22e
songjoonhyuk <songjoonhyuk@gmail.com>
2016-06-08 08:03:49 +0900

    forms.py 만들고 Post, Comment 모델 추가

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from blog.models import Post, Comment
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('content',)
